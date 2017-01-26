8cb3cb99ff4bbc43506d7bb05d28786308002058
Julius B <Julius B>
2017-01-26 16:55:38 +0100

    Collisions

--- src/com/github/infosimulators/physic/InertialSystem.java
+++ src/com/github/infosimulators/physic/InertialSystem.java
@@ -10,11 +10,19 @@ public class InertialSystem {
     protected int length;
     protected int width;
     public ArrayList<PhysicsObject> objectsInSpace;
+    protected float deltaTime;
 
     // Constructors
     public InertialSystem(int length, int width) {
         this.length = length;
         this.width = width;
+        objectsInSpace = new ArrayList<PhysicsObject>();
+    }
+
+    public InertialSystem(int length, int width, float deltaTime) {
+        this.length = length;
+        this.width = width;
+        this.deltaTime = deltaTime;
         objectsInSpace = new ArrayList<PhysicsObject>();
     }
 
@@ -25,35 +33,79 @@ public class InertialSystem {
     public int getWidth() {
         return this.width;
     }
-    public void registerPhysicsObject(PhysicsObject object){
+
+    public float getDeltaTime() {
+        return this.deltaTime;
+    }
+
+    public void setDeltaTime(float deltaTime) {
+        if (deltaTime <= 0) {
+            new Err("deltaTime can´t be below / or 0.");
+            return;
+        }
+        this.deltaTime = deltaTime;
+    }
+
+    public void registerPhysicsObject(PhysicsObject object) {
         objectsInSpace.add(object);
     }
-    public void unregisterPhysicsObject(PhysicsObject object){
+
+    public void unregisterPhysicsObject(PhysicsObject object) {
         objectsInSpace.remove(object);
     }
-    public void update() {
-        /*
-        foreach: find Gravitation between objectsInSpace
-                 append force to Object
-                 if hit : switch new OBJECTS
 
-        foreach: objectsInSpace move
-        */
+    public void update() {
+        addGravitationForces();
+        getCollisions();
     }
 
-    public void gravTick() {
+    public void addGravitationForces() {
         for (PhysicsObject object : objectsInSpace) {
             for (PhysicsObject other : objectsInSpace) {
                 if (object == other)
                     return;
-                object.appendForce(gravitation(object,other));
+                object.appendForce(gravitationForce);
             }
         }
     }
-    public static Vector2D gravitation(PhysicsObject a, PhysicsObject b ){
-        float force = (float) (6.673*(Math.pow(10,-11)) * ((a.mass * b.mass)/ Math.pow(Vector2D.subtract(a.position, b.position).magnitude(),2)));
-        System.out.println("force: " + force);
-        System.out.println("normalized: "+Vector2D.subtract(a.position, b.position).normalized());
-        return Vector2D.scale(Vector2D.subtract(a.position, b.position).normalized(),force);
+
+    public void getCollisions() {
+        for (PhysicsObject object : objectsInSpace) {
+            for (PhysicsObject other : objectsInSpace) {
+
+                /*If both objects overlap*/
+                Vector2D gravitationForce = gravitation(object, other);
+                if (Vector2D.subtract(object.position, other.position).magnitude() <= object.size + other.size) {
+                    /*If the gravitation is stronger than velocity*/
+                    if (Vector2D.subtract(object.velocity, other.velocity)
+                            .sqrMagnitude() < gravitationForce.sqrMagnitude() * deltaTime) {
+                        //Unite Objects and remove old
+                        ArrayList<Vector2D> forces = new ArrayList<Vector2D>();
+                        forces.addAll(object.forces);
+                        forces.addAll(other.forces);
+                        objectsInSpace.add(new PhysicsObject(
+                                Vectror2D.add(Vector2D.subtract(object.position, other.position).scale(0.5f),
+                                        object.position),
+                                Vector2D.add(object.velocity, other.velocity), object.mass + other.mass,
+                                (float) Math.cbrt(Math.pow(object.size, 3) + Math.pow(other.size, 3)), forces));
+                        objectsInSpace.remove(object);
+                        objectsInSpace.remove(other);
+                    } else { /*Else do and elastic collision*/
+                        Vector2D objectVelocityOld = object.velocity;
+                        Vector2D otherVelocityOld = other.velocity;
+                        object.velocity = otherVelocityOld;
+                        other.velocity = objectVelocityOld;
+                    }
+                }
+            }
+        }
+    }
+
+    public static Vector2D gravitation(PhysicsObject a, PhysicsObject b) {
+        float force = (float) (6.673 * (Math.pow(10, -11))
+                * ((a.mass * b.mass) / Math.pow(Vector2D.subtract(a.position, b.position).magnitude(), 2)));
+        /*System.out.println("force: " + force);
+        System.out.println("normalized: " + Vector2D.subtract(a.position, b.position).normalized());*/
+        return Vector2D.scale(Vector2D.subtract(a.position, b.position).normalized(), force);
     }
 }
--- src/com/github/infosimulators/physic/PhysicsObject.java
+++ src/com/github/infosimulators/physic/PhysicsObject.java
@@ -1,70 +1,114 @@
 package com.github.infosimulators.physic;
 
 import java.util.*;
+
 /**
- * 2D Vector class, stores positions, forces and velocities.
+ * Baseclass for all objects, manipulated by physics.
+ * stores position, forces, velocity, mass and Size
+ * It is assumed to be a sphere with the radius size with the mass located in the middle
  */
-public class PhysicsObject{
+public class PhysicsObject {
     public Vector2D position;
     public Vector2D velocity;
     protected ArrayList<Vector2D> forces;
     public float mass;
+    public float size;
+
+    public String toString() {
+        return "PhysicsObject: position=(" + position + "), velocity=(" + velocity + "), mass= " + mass + "', size=" + size;
+    }
 
 // Constructors
     public PhysicsObject() {
         forces = new ArrayList<Vector2D>();
-        position = new Vector2D(0f,0f);
-        velocity = new Vector2D(0f,0f);
+        position = new Vector2D(0f, 0f);
+        velocity = new Vector2D(0f, 0f);
+        this.size = 0;
         mass = 0f;
     }
+
     public PhysicsObject(Vector2D position) {
         forces = new ArrayList<Vector2D>();
         position = position;
-        velocity = new Vector2D(0f,0f);
+        velocity = new Vector2D(0f, 0f);
         mass = 0f;
+        size = 0f;
     }
-    public PhysicsObject(Vector2D position,Vector2D velocity) {
+
+    public PhysicsObject(Vector2D position, Vector2D velocity) {
         forces = new ArrayList<Vector2D>();
         this.position = position;
         this.velocity = velocity;
         mass = 0f;
+        size = 0f;
     }
-    public PhysicsObject(Vector2D position,Vector2D velocity, float mass) {
+
+    public PhysicsObject(Vector2D position, Vector2D velocity, float mass) {
+        forces = new ArrayList<Vector2D>();
+        this.position = position;
+        this.velocity = velocity;
+        this.size = 0f;
+        this.mass = mass;
+    }
+    public PhysicsObject(Vector2D position, Vector2D velocity, float mass, float size) {
         forces = new ArrayList<Vector2D>();
         this.position = position;
         this.velocity = velocity;
         this.mass = mass;
+        this.size = size;
     }
+
     public PhysicsObject(Vector2D position, float mass) {
         forces = new ArrayList<Vector2D>();
         this.position = position;
-        this.velocity = new Vector2D(0f,0f);;
+        this.velocity = new Vector2D(0f, 0f);
         this.mass = mass;
+        this.size = 0f;
     }
-    public PhysicsObject(Vector2D position,Vector2D velocity, float mass, ArrayList<Vector2D> forces) {
+    public PhysicsObject(Vector2D position, float mass, float size) {
+        forces = new ArrayList<Vector2D>();
+        this.position = position;
+        this.velocity = new Vector2D(0f, 0f);
+        this.mass = mass;
+        this.size = size;
+    }
+
+    public PhysicsObject(Vector2D position, Vector2D velocity, float mass, ArrayList<Vector2D> forces) {
         this.forces = forces;
         this.position = position;
         this.velocity = velocity;
         this.mass = mass;
+        this.size = 0;
     }
+    public PhysicsObject(Vector2D position, Vector2D velocity, float mass, float size, ArrayList<Vector2D> forces) {
+        this.forces = forces;
+        this.position = position;
+        this.velocity = velocity;
+        this.mass = mass;
+        this.size = size;
+    }
+
 // FUNCTIONS
-    public void appendForce(Vector2D force){
+    public void appendForce(Vector2D force) {
         forces.add(force);
     }
-    public void resetForces(){
+
+    public void resetForces() {
         forces = new ArrayList<Vector2D>();
     }
-    public void playoutForces(float deltaTime){
+
+    public void playoutForces(float deltaTime) {
         float sumX = 0f;
         float sumY = 0f;
 
-        for(Vector2D force: forces){
+        for (Vector2D force : forces) {
             sumX += force.x;
             sumY += force.y;
         }
-        velocity.add((sumX/mass)*deltaTime, (sumY/mass)*deltaTime);
+        velocity.add((sumX / mass) * deltaTime, (sumY / mass) * deltaTime);
     }
-    public void move(float deltaTime){
+
+    public void move(float deltaTime) {
         position = Vector2D.scale(velocity, deltaTime);
     }
 }
