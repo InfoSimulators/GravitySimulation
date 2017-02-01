ff03d20975679def35d3d64622bdd8ba86e781cc
Juli <julisep@outlook.de>
2017-02-01 09:33:27 +0100

    Update Vector2D.java

--- src/com/github/infosimulators/physic/Vector2D.java
+++ src/com/github/infosimulators/physic/Vector2D.java
@@ -1,82 +1,52 @@
 package com.github.infosimulators.physic;
 
 import java.lang.Math;
-import java.util.Vector;
 
 /**
  * 2D Vector class, stores positions, forces and velocities.
  */
-public class Vector2D {
-    public float x;
-    public float y;
-
+public class Vector2D extends Vector3D {
     @Override
     public String toString() {
-        return "[x='" + x + "', y=" + y + "', magnitude='" + magnitude() + "', direction='" + direction() + "']";
+        return "[x='" + x + "', y=" + y + "', magnitude='" + magnitude() + "']";
     }
 
     // Constructors
     public Vector2D(float x, float y) {
         this.x = x;
         this.y = y;
+        this.z = 0f;
     }
 
     public Vector2D() {
         this.x = 0f;
         this.y = 0f;
+        this.z = 0f;
     }
 
 //METHODES FOR ATTRIBUTESS
     /**
-     * Sets the x and y components.
-     * @param new x component
-     * @param new y component
-     * @return this
-     */
-    public Vector2D set(float x, float y) {
-        this.x = x;
-        this.y = y;
-        return this;
-    }
-
-    /**
-     * Sets the x and y components to the ones of another vector.
-     * @param another vector
-     * @return this
-     */
-    public void set(Vector2D v) {
-        this.x = v.x;
-        this.y = v.y;
-        return this;
-    }
-
-    /**
-     * returns the squared length of the vector.
-     * Much faster then magitude()
+     * returns the squared length of the vector. Much faster then magitude()
      *
      * @return squared length of the vector
-    */
+     */
+    @Override
     public float sqrMagnitude() {
         return x * x + y * y;
     }
 
     /**
      * @return length of the vector.
-    */
+     */
+    @Override
     public float magnitude() {
         return (float) Math.sqrt(x * x + y * y);
     }
 
     /**
-     * @return direction of the vector in degree.
-    */
-    public float direction() {
-        return (float) Math.atan(this.x / this.y);
-    }
-
-    /**
      * @return a normalized version of this vector. The vector is not changed.
-    */
+     */
+    @Override
     public Vector2D normalized() {
         Vector2D normalized = new Vector2D();
         float magnitude = magnitude();
@@ -88,30 +58,61 @@ public class Vector2D {
     }
 
     /**
-    * @return a copy of this vector
-    *
-    * @brief Get a copy of this vector
-    */
+     * @return a copy of this vector
+     *
+     * @brief Get a copy of this vector
+     */
+    @Override
     public Vector2D copy() {
         return new Vector2D(x, y);
     }
+
     //METHODES FOR MANIPULATION
+    /**
+     * Sets the x and y components.
+     *
+     * @param x new xcomponent
+     * @param y new y component
+     * @return this
+     */
+    public Vector2D Set(float x, float y) {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+
+    /**
+     * Sets the x and y components to the ones of another vector.
+     *
+     * @param v another vector
+     * @return this
+     */
+    public Vector2D Set(Vector2D v) {
+        this.x = v.x;
+        this.y = v.y;
+        return this;
+    }
 
     /**
      * method to add a vector to this vector;
+     *
+     * @param vector to add
      * @return this vector
-    */
-    public Vector2D add(Vector2D vector) {
+     */
+    public Vector2D Add(Vector2D vector) {
         this.x += vector.x;
         this.y += vector.y;
         return this;
     }
 
     /**
-     * method to add to this vector;
-     *  @return this vector
-    */
-    public Vector2D add(float x, float y) {
+     * method to add floats to this vector;
+     *
+     * @param x first float
+     * @param y second float
+     * @return this vector
+     */
+    public Vector2D Add(float x, float y) {
         this.x += x;
         this.y += y;
         return this;
@@ -119,9 +120,11 @@ public class Vector2D {
 
     /**
      * method to subtract a vector to this vector;
-     * @return this vector
-    */
-    public Vector2D subtract(Vector2D vector) {
+     *
+     * @param vector
+     * @return this vector changed
+     */
+    public Vector2D Subtract(Vector2D vector) {
         this.x -= vector.x;
         this.y -= vector.y;
         return this;
@@ -129,9 +132,12 @@ public class Vector2D {
 
     /**
      * method to subtract from this vector;
-     * @return ths vector
-    */
-    public Vector2D subtract(float x, float y) {
+     *
+     * @param x first float
+     * @param y second float
+     * @return this vector changed
+     */
+    public Vector2D Subtract(float x, float y) {
         this.x -= x;
         this.y -= y;
         return this;
@@ -140,64 +146,39 @@ public class Vector2D {
     /**
      * Multiplies a vector by a scalar.
      *
-     * @param n the number to multiply with the vector
-     * @return this vector
-    */
-    public Vector2D scale(float r) {
+     * @param r the number to multiply with the vector
+     * @return this vector changed
+     */
+    @Override
+    public Vector2D Scale(float r) {
         this.x *= r;
         this.y *= r;
         return this;
     }
 
     /**
-    *
-    * Divides a vector by a scalar.
-    *
-    * @brief Divide a vector by a scalar
-    * @param r the number by which to divide the vector
-    * @return this vector
-    */
-    public Vector2D div(float r) {
+     *
+     * Divides a vector by a scalar.
+     *
+     * @brief Divide a vector by a scalar
+     * @param r the number by which to divide the vector
+     * @return this vector changed
+     */
+    @Override
+    public Vector2D Div(float r) {
         x /= r;
         y /= r;
         return this;
     }
 
-    /**
-    * Calculates the dot product of two vectors.
-    *
-    * @param second vector
-    */
-    public float dot(Vector2D vector) {
-        return this.x * vector.x + this.y * vector.y;
-    }
-
-    /**
-    * @param x x component of the other vector
-    * @param y y component of the other vector
-    * @param z z component of the other vector
-    */
-    public float dot(float x, float y, float z) {
-        return this.x * x + this.y * y + this.z * z;
-    }
-
-    /**
-    * Calculates the cross product of this vector and another vector.
-    * @deprecated as there are no cross products for 2 dimensional vectors.
-    * Kept for compability with a future Vector3D class.
-    *
-    * @param v : another vector;
-    * @return null
-    */
-    public Vector2D cross(Vector2D v) {
-        return null;
-    }
 
     /**
      * normalizes this vector. The vector IS changed.
-     * @return this
-    */
-    public Vector2D normalize() {
+     *
+     * @return this vector changed
+     */
+    @Override
+    public Vector2D Normalize() {
         float magnitude = magnitude();
         if (magnitude != 0) {
             x /= magnitude;
@@ -207,16 +188,18 @@ public class Vector2D {
     }
 
     /**
-    *
-    * Set the magnitude of this vector to the value used for the <b>len</b> parameter.
-    *
-    * @param len the new length for this vector
-    * @brief Set the magnitude of the vector
-    * @return this
-    */
+     *
+     * Set the magnitude of this vector to the value used for the <b>len</b>
+     * parameter.
+     *
+     * @param len the new length for this vector
+     * @brief Set the magnitude of the vector
+     * @return this vector changed
+     */
+    @Override
     public Vector2D setMag(float len) {
-        normalize();
-        scale(len);
+        Normalize();
+        Scale(len);
         return this;
     }
 
@@ -226,25 +209,28 @@ public class Vector2D {
      *
      * @brief Rotate the vector by an angle,
      * @param theta the angle of rotation
-     * @return this
+     * @return this vector changed
      */
-    public Vector2D rotate(float theta) {
+    public Vector2D Rotate(float theta) {
         float temp = x;
-        x = x * Math.cos(theta) - y * Math.sin(theta);
-        y = temp * Math.sin(theta) + y * Math.cos(theta);
+        x = x * (float) Math.cos(theta) - y * (float) Math.sin(theta);
+        y = temp * (float) Math.sin(theta) + y * (float) Math.cos(theta);
         return this;
     }
 
     /**
-    *
-    * Linear interpolate the vector to another vector
-    *
-    * @brief Linear interpolate the vector to another vector
-    * @param v the vector to lerp to
-    * @param amt  The amount of interpolation; some value between 0.0 (old vector) and 1.0 (new vector). 0.1 is very near the old vector; 0.5 is halfway in between.
-    * @return this
-    */
-    public Vector2D lerp(Vector2D v, float amt) {
+     *
+     * Linear interpolate the vector to another vector
+     *
+     * @brief Linear interpolate the vector to another vector
+     * @param v the vector to lerp to
+     * @param amt The amount of interpolation; some value between 0.0 (old
+     * vector) and 1.0 (new vector). 0.1 is very near the old vector; 0.5 is
+     * halfway in between.
+     * @return this vector changed
+     */
+    
+    public Vector2D Lerp(Vector2D v, float amt) {
         x = x + (v.x - x) * amt;
         y = y + (v.y - y) * amt;
         return this;
@@ -252,95 +238,115 @@ public class Vector2D {
 
     //STATIC/PURE FUNCTIONS
     /**
-     * @return the sum of 2 vectors;
-    */
-    public static Vector2D add(Vector2D a, Vector2D b) {
+     * @param a
+     * @param b
+     * @return the sum of 2 vectors as new Vector;
+     */
+    public static Vector2D Add(Vector2D a, Vector2D b) {
         return new Vector2D(a.x + b.x, a.y + b.y);
     }
 
     /**
-     * @return the difference of two vectors
+     * @return the difference of two vectors as new Vector
      * @param a any variable of type Vector2D
      * @param b any variable of type Vector2D
-    */
-    public static Vector2D subtract(Vector2D a, Vector2D b) {
+     */
+    public static Vector2D Subtract(Vector2D a, Vector2D b) {
         return new Vector2D(a.x - b.x, a.y - b.y);
     }
 
     /**
-    * Multiplies a vector by a scalar.
-    *
-    * @brief Multiply a vector by a scalar
-    * @param v the vector to multiply by the scalar
-    * @param r the number to multiply with the vector
-    * @return a new vector that is v * r
-    */
-    public static Vector2D scale(Vector2D vector, float r) {
-        return new Vector2D(vector.x * r, vector.y * r);
+     * Multiplies a vector by a scalar.
+     *
+     * @brief Multiply a vector by a scalar
+     * @param v the vector to multiply by the scalar
+     * @param r the number to multiply with the vector
+     * @return a new vector that is v * r
+     */
+    public static Vector2D Scale(Vector2D v, float r) {
+        return new Vector2D(v.x * r, v.y * r);
     }
 
     /**
      * Divide a vector by a scalar and return the result in a new vector.
+     *
      * @param v the vector to divide by the scalar
      * @param r the scalar
      * @return a new vector that is v / r
      */
-    public static Vector2D div(Vector2D v, float r) {
+    public static Vector2D Div(Vector2D v, float r) {
         return new Vector2D(v.x / r, v.y / r);
     }
 
     /**
      *
-     * Calculates the distance between two points (considering a
-     * point as a vector object).
+     * Calculates the distance between two points (considering a point as a
+     * vector object).
      *
      * @param v1 any variable of type Vector2D
      * @param v2 any variable of type Vector2D
      * @return the Euclidean distance between v1 and v2
      */
-    static public float dist(Vector2D v1, Vector2D v2) {
+    static public float Distance(Vector2D v1, Vector2D v2) {
         float dx = v1.x - v2.x;
         float dy = v1.y - v2.y;
-        float dz = v1.z - v2.z;
-        return (float) Math.sqrt(dx * dx + dy * dy + dz * dz);
+        return (float) Math.sqrt(dx * dx + dy * dy);
     }
 
     /**
      * method to get the dot product (scalar product) of 2 vectors;
      *
+     * @param a
+     * @param b
      * @return the dot product
      * @brief Calculate the dot product of two vectors
      */
-    public static float dot(Vector2D a, Vector2D b) {
+    public static float Dot(Vector2D a, Vector2D b) {
         return a.x * b.x + a.y * b.y;
     }
 
     /**
      * @param v1 any variable of type Vector2D
      * @param v2 any variable of type Vector2D
-     * @param target PVector to store the result
      * @return null
      */
-    public static Vector2D cross(Vector2D v1, Vector2D v2) {
+    public static Vector2D Cross(Vector2D v1, Vector2D v2) {
         return null;
     }
 
     /**
-    * Linear interpolate between two vectors (@return a new Vector2D object)
-    * @param v1 the vector to start from
-    * @param v2 the vector to lerp to
-    */
+     * Linear interpolate between two vectors (@return a new Vector2D object)
+     *
+     * @param v1 the vector to start from
+     * @param v2 the vector to lerp to
+     * @param amt
+     * @return
+     */
     public static Vector2D lerp(Vector2D v1, Vector2D v2, float amt) {
         Vector2D v = v1.copy();
-        v.lerp(v2, amt);
+        v.Lerp(v2, amt);
         return v;
     }
 
     /**
-    * @return a new vector based on
-    * @param angle ;
-    *
-    */
+     * Calculates the angle between two vectors in radiants
+     *
+     * @param vector1
+     * @param vector2
+     * @return angle between two vectors in radiants
+     */
+    public static float AngleBetween(Vector2D vector1, Vector2D vector2) {
+        float sin = vector1.x * vector2.y - vector2.x * vector1.y;
+        float cos = vector1.x * vector2.x + vector1.y * vector2.y;
+
+        return (float) Math.atan2(sin, cos);
+    }
+
+    /**
+     * @return a new vector based on
+     * @param angle ;
+     *
+     */
     static public Vector2D fromAngle(float angle) {
         return new Vector2D((float) Math.cos(angle), (float) Math.sin(angle));
     }
