d25379516a4b487e62a23b48113a0c2a658cdf92
Jannis Jorre <Jannis Jorre>
2017-02-17 11:08:55 +0100

    Setup base GeneticTrainer class layout

--- src/com/github/infosimulators/genetictrainer/GeneticTrainer.java
+++ src/com/github/infosimulators/genetictrainer/GeneticTrainer.java
@@ -4,21 +4,68 @@ package com.github.infosimulators.genetictrainer;
  * Trainer class for finding optimal parameters.
  */
 public class GeneticTrainer {
-
-	/**
-	 * Auto-generated constructor
-	 */
-	public GeneticTrainer() {
-		// TODO Auto-generated constructor
+	
+	public enum ParameterTypes {
+		BOOLEAN, BYTE, SHORT, INT, LONG, FLOAT, DOUBLE
 	}
 	
+	private ParameterTypes[] format;
+	private int geneCount;
+
 	/**
-	 * Trains with given parameters
+	 * constructor
 	 */
-	public void train(Parameter[] trainableParameters) {
+	public GeneticTrainer(ParameterTypes[] format) {
+		setFormat(format);
+	}
+
+	/**
+	 * Trains
+	 */
+	public void train() {
 		// train parameters
 	}
 	
-	public abstract class Parameter {}
+	public void setFormat(ParameterTypes[] format) {
+		updateGeneCount(format);
+		this.format = format;
+	}
+	
+	private void updateGeneCount(ParameterTypes[] format) {
+		this.geneCount = 0;
+		for (ParameterTypes p : format) {
+			switch (p) {
+			case BOOLEAN:
+				geneCount++;
+				break;
+			case BYTE:
+				geneCount += 8;
+				break;
+			case SHORT:
+				geneCount += 16;
+				break;
+			case INT:
+				geneCount += 32;
+				break;
+			case LONG:
+				geneCount += 64;
+				break;
+			case FLOAT:
+				geneCount += 32;
+				break;
+			case DOUBLE:
+				geneCount += 64;
+				break;
+			}
+		}
+	}
+	
+	public ParameterTypes[] getFormat() {
+		return format;
+	}
+	
+	public int getGeneCount() {
+		return geneCount;
+	}
 
 }
