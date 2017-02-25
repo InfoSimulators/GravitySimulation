fe473ca2ccc477ad5cf2ec74729a3ffc5d28dfdd
jeyj0 <jeyj0.plus@gmail.com>
2017-02-25 21:58:59 +0100

    Added IDRegistry and IDd class base functionality

---
+++ src/com/github/infosimulators/IDRegistry.java
@@ -0,0 +1,25 @@
+package com.github.infosimulators;
+
+public class IDRegistry {
+	
+	private static int current = 0;
+	
+	public static int nextID() {
+		return current++;
+	}
+	
+	public abstract class IDd {
+
+		private int id;
+		
+		public IDd() {
+			id = IDRegistry.nextID();
+		}
+		
+		public int getID() {
+			return id;
+		}
+		
+	}
+	
+}
