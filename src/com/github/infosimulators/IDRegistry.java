dc897e4aeb266c625c9578f51bc48e19e692f79f
jeyj0 <jeyj0.plus@gmail.com>
2017-02-26 00:00:45 +0100

    Added comments to IDRegistry.java

--- src/com/github/infosimulators/IDRegistry.java
+++ src/com/github/infosimulators/IDRegistry.java
@@ -1,25 +1,59 @@
 package com.github.infosimulators;
 
 public class IDRegistry {
-	
+
+	/**
+	 * Counter variable to keep track of already given IDs.
+	 */
 	private static int current = 0;
-	
-	public static int nextID() {
+
+	/**
+	 * @return The next available ID
+	 */
+	private static int nextID() {
 		return current++;
 	}
-	
+
+	/**
+	 * @return The next ID that will be given out
+	 */
+	public static int getNextID() {
+		return current + 1;
+	}
+
+	/**
+	 * @return The last ID that was given out to an object.
+	 */
+	public static int getLastID() {
+		return current;
+	}
+
+	/**
+	 * Superclass for all objects that should get an ID. Automatically assigns a
+	 * unique ID for the object.
+	 */
 	public static abstract class IDd {
 
+		/**
+		 * The unique ID of the object.
+		 */
 		private int id;
-		
+
+		/**
+		 * Creates a new IDd and registered object. Automatically assigns the
+		 * next available ID.
+		 */
 		public IDd() {
 			id = IDRegistry.nextID();
 		}
-		
+
+		/**
+		 * @return The unique ID of this object.
+		 */
 		public int getID() {
 			return id;
 		}
-		
+
 	}
-	
+
 }
